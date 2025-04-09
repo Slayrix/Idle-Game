623c4d0e36ab4e1d3c2cf534674614d26b99e3db
Slayrix <52615772+Slayrix@users.noreply.github.com>
2025-04-08 20:06:20 -0500

    Added Chars.py

--- Dev/Game/event.py
+++ Dev/Game/event.py
@@ -1,46 +1,4 @@
-import pygame as pg, menu, vars.listVars as listVars, vars.textBoxVars as textBoxVars
-
-pgAlphabet = {
-    pg.K_a: "a", 
-    pg.K_b: "b", 
-    pg.K_c: "c", 
-    pg.K_d: "d", 
-    pg.K_e: "e", 
-    pg.K_f: "f", 
-    pg.K_g: "g", 
-    pg.K_h: "h", 
-    pg.K_i: "i", 
-    pg.K_j: "j", 
-    pg.K_k: "k", 
-    pg.K_l: "l", 
-    pg.K_m: "m", 
-    pg.K_n: "n", 
-    pg.K_o: "o", 
-    pg.K_p: "p", 
-    pg.K_q: "q", 
-    pg.K_r: "r", 
-    pg.K_s: "s",  
-    pg.K_t: "t", 
-    pg.K_u: "u", 
-    pg.K_v: "v", 
-    pg.K_w: "w", 
-    pg.K_x: "x", 
-    pg.K_y: "y", 
-    pg.K_z: "z"
-}
-
-pgNums = {
-    pg.K_0: "0",
-    pg.K_1: "1",
-    pg.K_2: "2",
-    pg.K_3: "3",
-    pg.K_4: "4",
-    pg.K_5: "5",
-    pg.K_6: "6",
-    pg.K_7: "7",
-    pg.K_8: "8",
-    pg.K_9: "9",
-}
+import pygame as pg, menu, vars.listVars as listVars, vars.textBoxVars as textBoxVars, chars
 
 def eventCheck(running):
     for event in pg.event.get():
@@ -86,14 +44,14 @@ def checkIfKeyPressed(event):
             textBoxVars.cheatsTextBox.delTextFromTextBox()
 
 def checkIfletterPressed(event):
-    if event.type == pg.KEYDOWN and event.key in pgAlphabet:
-        return [True, pgAlphabet[event.key]]
+    if event.type == pg.KEYDOWN and event.key in chars.pgAlphabet:
+        return [True, chars.pgAlphabet[event.key]]
     else:
         return False
 
 def checkIfNumPressed(event):
-    if event.type == pg.KEYDOWN and event.key in pgNums:
-        return [True, pgNums[event.key]]
+    if event.type == pg.KEYDOWN and event.key in chars.pgNums:
+        return [True, chars.pgNums[event.key]]
     else:
         return False
 
