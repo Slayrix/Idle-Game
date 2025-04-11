2efb0953a3b008ac2406516b57f0d1fc8b7ec8c1
Slayrix <52615772+Slayrix@users.noreply.github.com>
2025-04-10 21:58:44 -0500

    Improved var files readability

--- Dev/Game/vars/upgradeVars.py
+++ Dev/Game/vars/upgradeVars.py
@@ -1,7 +1,46 @@
 import classes.upgradeClass as upgradeClass, vars.currencyVars as currencyVars
 
-bigBangUpgrade = upgradeClass.genUpgrade(10, currencyVars.energy, 0, currencyVars.matter, "+", .01, currencyVars.matter, "+", 10)
-genEnergyUpgrade = upgradeClass.genUpgrade(.05, currencyVars.matter, .1, currencyVars.energy, "+", 1)
-matterGenUpgrade = upgradeClass.genUpgrade(.15, currencyVars.matter, 6, currencyVars.matter, "*", 2, currencyVars.matter, "+", 10)
+#Gen Upgrades
+cost = 10
+currencyCost = currencyVars.energy
+growthRate = 0
+increaseGenPerSecondCurrency = currencyVars.matter
+operation1 = "+"
+increaseGenPerSecondAmount = .01
+increaseCostPerGenCurrency = currencyVars.matter
+operation2 = "+"
+increaseCostPerGenAmount = 10
+bigBangUpgrade = upgradeClass.genUpgrade(cost, currencyCost, growthRate, increaseGenPerSecondCurrency, operation1, increaseGenPerSecondAmount, increaseCostPerGenCurrency, operation2, increaseCostPerGenAmount)
 
-genEnergyUpgradeBuff = upgradeClass.upgradeBuff(.5, currencyVars.matter, genEnergyUpgrade, "increaseGenPerSecondAmount", "*", 2)
+cost = .05
+currencyCost = currencyVars.matter
+growthRate = .1
+increaseGenPerSecondCurrency = currencyVars.energy
+operation1 = "+"
+increaseGenPerSecondAmount = 1
+increaseCostPerGenCurrency = None
+operation2 = None
+increaseCostPerGenAmount = None
+genEnergyUpgrade = upgradeClass.genUpgrade(cost, currencyCost, growthRate, increaseGenPerSecondCurrency, operation1, increaseGenPerSecondAmount, increaseCostPerGenCurrency, operation2, increaseCostPerGenAmount)
+
+cost = .15
+currencyCost = currencyVars.matter
+growthRate = 6
+increaseGenPerSecondCurrency = currencyVars.matter
+operation1 = "*"
+increaseGenPerSecondAmount = 2
+increaseCostPerGenCurrency = currencyVars.matter
+operation2 = "+"
+increaseCostPerGenAmount = 10
+matterGenUpgrade = upgradeClass.genUpgrade(cost, currencyCost, growthRate, increaseGenPerSecondCurrency, operation1, increaseGenPerSecondAmount, increaseCostPerGenCurrency, operation2, increaseCostPerGenAmount)
+
+
+#Upgrade Buffs
+cost = .5
+currencyCost = currencyVars.matter
+upgradeBuffed = genEnergyUpgrade
+upgradeVarBuffed = "increaseGenPerSecondAmount"
+operation = "*"
+amountIncrease = 2
+growthRate = 0
+genEnergyUpgradeBuff = upgradeClass.upgradeBuff(cost, currencyCost, upgradeBuffed, upgradeVarBuffed, operation, amountIncrease, growthRate)
